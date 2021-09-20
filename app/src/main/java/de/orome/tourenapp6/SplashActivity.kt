b356613f89bdf52c7801ba8ddefd20bb309cad4d
RHerrmann <reinhard-ulrich.herrmann@web.de>
2021-09-20 11:44:08 +0200

    Step 1.2    Erstellen einer Animation für SplashScreen             Splashscreen soll Fullscreen werden             NoActionBar-Theme hinzugefügt und in Manifest zur SplashActivity zugefügt             Animation erstellt definitionen mit translate (Bewegung) und alpha (Sichtbarwerden)             animationListner eingeführt. Dieser ruft neue Activity auf

--- app/src/main/java/de/orome/tourenapp6/SplashActivity.kt
+++ app/src/main/java/de/orome/tourenapp6/SplashActivity.kt
@@ -1,7 +1,15 @@
 package de.orome.tourenapp6
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import de.orome.tourenapp6.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -11,5 +19,41 @@ class SplashActivity : AppCompatActivity() {
 
         val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
+
+        // Anpassung, dass in allen Versionen Fullscreen funktioniert
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        // Ende Anpassung
+        // TextViewanimieren
+        val splashAnimation = AnimationUtils.loadAnimation(this,R.anim.anim_splash)
+        splashBinding.tvSplashScreenTitle.animation = splashAnimation
+        // Listener einführen, um Animation in der Mitte des Bildschirmes zu halten
+        splashAnimation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                // eine Sekunde nach Ende der Animation zur StarrtActivity springen
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity,
+                    MainActivity::class.java))
+                    // Splashscreen endgültig beenden
+                    finish()
+                },1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+        })
     }
 }
