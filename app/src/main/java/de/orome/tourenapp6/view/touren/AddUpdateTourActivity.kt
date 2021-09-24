920f921782dd58f559418bb9b1761e03582f3b37
RHerrmann <reinhard-ulrich.herrmann@web.de>
2021-09-24 09:26:12 +0200

    Kap. 29.1   Designing AddUpdateTourActivity part 1             Layout erstellt Theme zugeordnet,             Label und mehrere TextInputLayout hinzugefügt             Felder sind noch nicht vollständig

--- app/src/main/java/de/orome/tourenapp6/view/touren/AddUpdateTourActivity.kt
+++ app/src/main/java/de/orome/tourenapp6/view/touren/AddUpdateTourActivity.kt
@@ -1,12 +1,40 @@
 package de.orome.tourenapp6.view.touren
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import de.orome.tourenapp6.R
+import de.orome.tourenapp6.databinding.ActivityAddUpdateTourBinding
 
 class AddUpdateTourActivity : AppCompatActivity() {
+
+    private lateinit var mbinding: ActivityAddUpdateTourBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_tour)
+
+        mbinding = ActivityAddUpdateTourBinding.inflate(layoutInflater)
+        setUpActionBar()
+        setContentView(mbinding.root)
+    }
+
+
+
+
+
+
+
+
+    @SuppressLint("RestrictedApi")
+    fun setUpActionBar(){
+        setSupportActionBar(mbinding.toolbarAddUpdateTourActivity)
+        // Back-Button anzeigen
+        supportActionBar?.setDefaultDisplayHomeAsUpEnabled(true)
+        //mbinding.toolbarAddUpdateTourActivity.setTitleTextColor(0xFFFFFFF)
+        // ClickListener für BackButton
+        mbinding.toolbarAddUpdateTourActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
