8a92a024fa59e6ede72024efaa27c55da5f4273b
RHerrmann <reinhard-ulrich.herrmann@web.de>
2021-09-27 09:18:46 +0200

    Kap. 45.4   ROOM TourenRepository erstellt

---
+++ app/src/main/java/de/orome/tourenapp6/model/repository/TourenRepository.kt
@@ -0,0 +1,17 @@
+package de.orome.tourenapp6.model.repository
+
+import androidx.annotation.WorkerThread
+import de.orome.tourenapp6.model.database.daos.TourDao
+import de.orome.tourenapp6.model.database.entities.Tour
+
+class TourenRepository(private val tourDao: TourDao) {
+
+    // By default Room runs suspend queries off the main thread, therefore, we don't need to
+    // implement anything else to ensure we're not doing long running database work
+    // off the main thread.
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(tour: Tour) {
+        tourDao.insertInitialTour(tour)
+    }
+}
