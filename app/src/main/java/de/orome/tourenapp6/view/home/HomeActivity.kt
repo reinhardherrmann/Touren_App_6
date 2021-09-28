52aad319dd0e53ab4c6d723e5af06266e29823ee
RHerrmann <reinhard-ulrich.herrmann@web.de>
2021-09-28 12:27:54 +0200

    Kap. 50.0   Neustrukturierung der GUI             alle Fragments und Activities gelöscht bis auf HomeActivity und             SplashActivity
                Neu erstellt: HomeActivity und TourenActivity und Navigationgraph

--- app/src/main/java/de/orome/tourenapp6/view/home/HomeActivity.kt
+++ app/src/main/java/de/orome/tourenapp6/view/home/HomeActivity.kt
@@ -1,12 +1,37 @@
 package de.orome.tourenapp6.view.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import de.orome.tourenapp6.R
+import de.orome.tourenapp6.databinding.ActivityHomeBinding
+import de.orome.tourenapp6.view.touren.TourenActivity
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mBinding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        mBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.btnCallTouren.setOnClickListener(this)
+    }
+
+
+
+    // ---------- OnClickListener für alle Buttons und Views ------------------------
+    override fun onClick(v: View?) {
+        if(v != null){
+            when(v.id){
+                R.id.btnCallTouren -> {
+                    startActivity(Intent(this,TourenActivity::class.java))
+                }
+            }
+        }
+
     }
 }
