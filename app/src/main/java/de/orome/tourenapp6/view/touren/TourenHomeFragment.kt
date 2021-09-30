0ea16750d77fb0e7c82ced773fbdf609dacdfcb5
RHerrmann <reinhard-ulrich.herrmann@web.de>
2021-09-30 08:36:39 +0200

    Kap. 50.2   TourenFragments mit ViewBinding ausgerüstet             fragment_new_tour gestaltet

--- app/src/main/java/de/orome/tourenapp6/view/touren/TourenHomeFragment.kt
+++ app/src/main/java/de/orome/tourenapp6/view/touren/TourenHomeFragment.kt
@@ -3,20 +3,20 @@ package de.orome.tourenapp6.view.touren
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import de.orome.tourenapp6.R
+import de.orome.tourenapp6.databinding.FragmentTourenHomeBinding
 
 
 class TourenHomeFragment : Fragment() {
 
-    private lateinit var rootView: View
+    private lateinit var mBinding: FragmentTourenHomeBinding
 
-    private lateinit var btnTourDetails: Button
-    private lateinit var fabNewTour: FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,22 +24,20 @@ class TourenHomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-       rootView = inflater.inflate(R.layout.fragment_touren_home, container, false)
+        mBinding = FragmentTourenHomeBinding.inflate(layoutInflater)
+        val view = mBinding.root
 
         val navControler = findNavController(this)
 
-        btnTourDetails = rootView.findViewById(R.id.btn_tour_details)
-        fabNewTour = rootView.findViewById(R.id.fab_new_tour)
-
-        btnTourDetails.setOnClickListener {
+        mBinding.btnTourDetails.setOnClickListener {
             navControler.navigate(R.id.action_tourenHomeFragment_to_tourenDetailFragment)
         }
 
-        fabNewTour.setOnClickListener {
+        mBinding.fabNewTour.setOnClickListener {
             navControler.navigate(R.id.action_tourenHomeFragment_to_newTourFragment)
         }
 
-        return rootView
+        return view
 
     }
 
