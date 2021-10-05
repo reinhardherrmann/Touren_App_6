fecce6085ad5f50f499eb00f3697a3af3301f85f
RHerrmann <reinhard-ulrich.herrmann@web.de>
2021-10-05 17:48:38 +0200

    Kap 51.2    Adapter für TourenListe RecyclerView erstellt

---
+++ app/src/main/java/de/orome/tourenapp6/adapter/TourenListAdapter.kt
@@ -0,0 +1,52 @@
+package de.orome.tourenapp6.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import de.orome.tourenapp6.databinding.ItemTourenListeBinding
+import de.orome.tourenapp6.model.database.entities.Tour
+
+class TourenListAdapter(private val fragment: Fragment): RecyclerView.Adapter<TourenListAdapter.ViewHolder>() {
+
+    private var touren: List<Tour> = listOf() // als leere Liste initialisieren
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val mBinding: ItemTourenListeBinding = ItemTourenListeBinding.inflate(LayoutInflater.from(
+            fragment.context),
+            parent,
+            false)
+        return ViewHolder(mBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val tour = touren[position]
+        holder.tvTourNummer.text = tour.tourNummer
+        holder.tvTourDatum.text = tour.tourDatum
+        holder.tvTourAnzStopps.text = "4"
+        holder.tvTourStatus.text = "Rückfahrt"
+    }
+
+    override fun getItemCount(): Int {
+        return touren.size
+    }
+
+
+    fun tourenListe(list: List<Tour>){
+        touren = list
+        notifyDataSetChanged()
+    }
+
+
+
+
+    class ViewHolder(view: ItemTourenListeBinding): RecyclerView.ViewHolder(view.root) {
+        // Holds the TextViews
+        val tvTourDatum = view.tourItemTvTourDatum
+        val tvTourNummer = view.tourItemTvTourNummer
+        val tvTourAnzStopps = view.tourItemTvAnzStopps
+        val tvTourStatus = view.tourItemTvStatus
+    }
+
+
+}
